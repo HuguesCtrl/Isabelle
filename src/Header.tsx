cf7423f24208e91e3db612c42f96fbbe74af2977
Hugues Collet <hc.pro@outlook.fr>
2024-04-18 11:32:08 +0200

    Titre header réduit

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <div className="header">
         <h1>Les négociales</h1>
         <h2>11 Avril 2024</h2>
-        <h3>Pour télécharger une photo cliquez sur l'icône <img src={DL} height="30" width="30"/> </h3>
+        <h3>Pour télécharger une photo cliquez sur l'icône <img src={DL} height="20" width="20"/> </h3>
     </div>
   )
 }
